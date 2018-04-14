b1ea38cea312e5fcd7bf3811514d2414c06ed2fc
lixiaodaoaaa <lixiaodaoaaa@126.com>
2018-04-15 01:25:05 +0800

    add  gradient practice  to project

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice1DrawColorView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice1DrawColorView.java
@@ -1,12 +1,21 @@
 package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
-import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.ComposeShader;
+import android.graphics.LightingColorFilter;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.hencoder.hencoderpracticedraw1.R;
 
 public class Practice1DrawColorView extends View{
     
@@ -44,14 +53,24 @@ public class Practice1DrawColorView extends View{
     @Override
     protected void onDraw(Canvas canvas){
         super.onDraw(canvas);
-
-//        练习内容：使用 canvas.drawColor() 方法把 View 涂成黄色
-//        黄色： Color.YELLOW
+        paint=new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setStyle(Paint.Style.FILL);
         
+        Shader shader=new ComposeShader(getBitmapShader01(), getBitmapShader02(), PorterDuff.Mode.SRC_OVER);
+        paint.setShader(shader);
+        float radius=mWidth / 2 - mWidth / 5;
+        canvas.drawCircle(mWidth / 2, mHeight / 2, radius, paint);
+    
+    }
+    
+    private BitmapShader getBitmapShader01(){
+        Bitmap bitmap=BitmapFactory.decodeResource(getResources(), R.drawable.batman);
         
-        
-        
-        
-        canvas.drawColor(Color.YELLOW);
+        return new BitmapShader(bitmap, Shader.TileMode.MIRROR, Shader.TileMode.MIRROR);
+    }
+    
+    private BitmapShader getBitmapShader02(){
+        Bitmap bitmap=BitmapFactory.decodeResource(getResources(), R.drawable.sample_path);
+        return new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
     }
 }
